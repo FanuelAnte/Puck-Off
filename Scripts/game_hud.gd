d66bdd3054d8a2227b6dd4a1c26aacbb815fa113
Fanuel Anteneh <apexfanuel@gmail.com>
2024-11-10 23:46:57 +0300

    New Palette

--- Scripts/game_hud.gd
+++ Scripts/game_hud.gd
@@ -6,3 +6,18 @@ func _ready() -> void:
 	
 func _process(delta: float) -> void:
 	pass
+
+func vibrate() -> void:
+	Input.vibrate_handheld(50, 0.5)
+
+func _on_d_pad_left_pressed() -> void:
+	vibrate()
+	
+func _on_d_pad_right_pressed() -> void:
+	vibrate()
+
+func _on_a_button_pressed() -> void:
+	vibrate()
+
+func _on_z_button_pressed() -> void:
+	vibrate()
